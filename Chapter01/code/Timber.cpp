d27650d0dc9be88cc7306f4de16aea6315079bce
Ryan Hermle <W1622454@arc.losrios.edu>
2022-08-09 12:00:36 -0700

    Commiting from Win10

--- Chapter01/code/Timber.cpp
+++ Chapter01/code/Timber.cpp
@@ -1,16 +1,18 @@
 // Include important C++ libraries here
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 // Make code easier to type with "using namespace"
 using namespace sf;
+using namespace std;
 
 int main()
 {
 	// Create a video mode object
-	VideoMode vm(VideoMode::getDesktopMode().width, VideoMode::getDesktopMode().height);
+	VideoMode vm(1920, 1080);
 
 	// Create and open a window for the game
-	RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+	RenderWindow window(vm, "Timber!!!", Style::Default);
 
 	// Create a texture to hold a graphic on the GPU
 	Texture textureBackground;
