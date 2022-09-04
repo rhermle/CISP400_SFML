02810ec6f3bf4c9cdf110ca4d4b016eaca3d28de
Ryan Hermle <W1622454@arc.losrios.edu>
2022-09-04 14:52:17 -0700

    Test3

--- Chapter05/code/Timber.cpp
+++ Chapter05/code/Timber.cpp
@@ -23,7 +23,7 @@ int main()
 	VideoMode vm(1920, 1080);
 
 	// Create and open a window for the game
-	RenderWindow window(vm, "Timber", Style::Default);
+	RenderWindow window(vm, "Timber!!!!!!!!!", Style::Default);
 
 	// Create a texture to hold a graphic on the GPU
 	Texture textureBackground;
